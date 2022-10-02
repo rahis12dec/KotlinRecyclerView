5ae01dfa37e67b8cf0842fed4e29785e28dd3e96
rahis_kumar <rahis12dec@gmail.com>
2022-10-02 14:03:08 +0530

    second commit for recycler view data

--- app/src/main/java/com/anode/kotlinrecyclerview/MainActivity.kt
+++ app/src/main/java/com/anode/kotlinrecyclerview/MainActivity.kt
@@ -2,10 +2,16 @@ package com.anode.kotlinrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showRecyclerData.setOnClickListener {
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/anode/kotlinrecyclerview/RecyclerViewActivity.kt
@@ -0,0 +1,16 @@
+package com.anode.kotlinrecyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_recycler_view.*
+
+class RecyclerViewActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+        recyclerViewData
+
+    }
+
+}
